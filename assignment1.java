4a61990fc903d133781b29a266ee77f195c090dd
Whyaziz <89498878+Whyaziz@users.noreply.github.com>
2023-02-26 14:21:25 +0700

    Update assignment1.java

--- assignment1.java
+++ assignment1.java
@@ -2,16 +2,41 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class assignment1 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        ArrayList<String> belanja = new ArrayList<String>(Arrays.asList("Cabai","Bayam","Bawang","Tempe","Beras"));
+        String[] Barang = {"cabai", "bayam", "bawang", "tempe", "beras"};
+        int[] harga = {3000, 2000, 3000, 5000, 20000};
+        int uangDina = 50000;
 
-        System.out.println("Daftar belanja Dina adalah sebagai berikut : "+belanja);
-        System.out.println("===Catatan Belanja===");
-        for (String i : belanja){
-            System.out.println(i);
+        ArrayList<String> barangDibeli = new ArrayList<>();
+
+        int totalBelanja = 0;
+        for (int i = 0; i < Barang.length; i++) {
+            if (Barang[i].equals("cabai")) {
+                continue;
+            }
+
+            int hargaBarang = harga[i];
+            totalBelanja += hargaBarang;
+
+            barangDibeli.add(Barang[i]);
         }
 
+        barangDibeli.add("ayam");
+        barangDibeli.add("minyak");
+        totalBelanja += harga[4] + harga[2];
+
+        System.out.println("Daftar catatan Dina: " + Arrays.toString(Barang));
+
+        System.out.println("Barang yang dibeli Dina: " + barangDibeli);
+
+        System.out.println("Total belanja Dina: " + totalBelanja);
+        if (totalBelanja < uangDina) {
+            System.out.println("Kurang " + (uangDina - totalBelanja));
+        } else if (totalBelanja == uangDina) {
+            System.out.println("Pas");
+        } else {
+            System.out.println("Lebih " + (totalBelanja - uangDina));
+        }
     }
-    
 }
